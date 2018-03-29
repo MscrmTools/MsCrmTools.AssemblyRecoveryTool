d228f0a02657da9c6d3df33bf7b4147aacdeb0af
MscrmTools <tanguy92@hotmail.com>
2018-03-29 13:12:02 +0200

    Fixed #1 Retrieve all assemblies with identical assembly name (but different versions)

--- MsCrmTools.AssemblyRecoveryTool/MainControl.cs
+++ MsCrmTools.AssemblyRecoveryTool/MainControl.cs
@@ -11,35 +11,7 @@ namespace MsCrmTools.AssemblyRecoveryTool
 {
     public partial class MainControl : PluginControlBase, IGitHubPlugin, IHelpPlugin
     {
-        public AssemblyManager Manager
-        {
-            get;
-            private set;
-        }
-
-        public string RepositoryName
-        {
-            get
-            {
-                return "MsCrmTools.AssemblyRecoveryTool";
-            }
-        }
-
-        public string UserName
-        {
-            get
-            {
-                return "MscrmTools";
-            }
-        }
-
-        public string HelpUrl
-        {
-            get
-            {
-                return "https://github.com/MscrmTools/MsCrmTools.AssemblyRecoveryTool/wiki";
-            }
-        }
+        private AssemblyManager manager;
 
         #region Constructor
 
@@ -51,7 +23,7 @@ namespace MsCrmTools.AssemblyRecoveryTool
             InitializeComponent();
 
             // Execute code automatically when plugin is loaded
-            this.Enter += MainControl_Enter;
+            Enter += MainControl_Enter;
         }
 
         /// <summary>
@@ -59,7 +31,7 @@ namespace MsCrmTools.AssemblyRecoveryTool
         /// </summary>
         /// <param name="sender">Instance of class <see cref="MainControl"/></param>
         /// <param name="e">Event arguments</param>
-        private void MainControl_ConnectionUpdated(object sender, PluginControlBase.ConnectionUpdatedEventArgs e)
+        private void MainControl_ConnectionUpdated(object sender, ConnectionUpdatedEventArgs e)
         {
             ExecuteMethod(RetrieveAssemblies);
         }
@@ -71,79 +43,52 @@ namespace MsCrmTools.AssemblyRecoveryTool
         /// <param name="e">Event aguments</param>
         private void MainControl_Enter(object sender, EventArgs e)
         {
-            if (sender != null)
+            if (sender is MainControl control)
             {
-                if (sender is MainControl)
-                {
-                    var plugin = ((MainControl)sender);
-                    // In case if connection updated on main application, update assemblies list inside the plugin
-                    plugin.ConnectionUpdated += MainControl_ConnectionUpdated;
+                var plugin = control;
+                // In case if connection updated on main application, update assemblies list inside the plugin
+                plugin.ConnectionUpdated += MainControl_ConnectionUpdated;
 
-                    if (plugin.Service != null)
-                    {
-                        // Execute assemblies retrieve only if Service object is set for correct sender.
-                        // This will help plugin act predicatable when it was loaded in offline mode;
-                        // Plugin will not insist on connecting to server. Will scinetly obey instead.
-                        ExecuteMethod(RetrieveAssemblies);
-                    }
+                if (plugin.Service != null)
+                {
+                    // Execute assemblies retrieve only if Service object is set for correct sender.
+                    // This will help plugin act predicatable when it was loaded in offline mode;
+                    // Plugin will not insist on connecting to server. Will scinetly obey instead.
+                    ExecuteMethod(RetrieveAssemblies);
                 }
             }
         }
 
         #endregion Constructor
 
-        #region Methods
+        #region Interfaces implementation
+
+        public string RepositoryName => "MsCrmTools.AssemblyRecoveryTool";
+
+        public string UserName => "MscrmTools";
+
+        public string HelpUrl => "https://github.com/MscrmTools/MsCrmTools.AssemblyRecoveryTool/wiki";
+
+        #endregion Interfaces implementation
+
+        #region Events
 
         private void TsbCloseClick(object sender, EventArgs e)
         {
             CloseTool();
         }
 
-        #endregion Methods
-
-        public void RetrieveAssemblies()
-        {
-            // Initalizing plugin wide AssemblyManager instanace
-            Manager = new AssemblyManager(Service);
-
-            WorkAsync(new WorkAsyncInfo
-            {
-                Message = "Loading assemblies...",
-                Work = (bw, e) =>
-                {
-                    e.Result = Manager.RetrieveAssemblies();
-                },
-                PostWorkCallBack = e =>
-                {
-                    listView_Assemblies.Items.Clear();
-
-                    var list = (List<Entity>)e.Result;
-
-                    foreach (Entity pAssembly in list)
-                    {
-                        var item = new ListViewItem(pAssembly["name"].ToString());
-                        item.SubItems.Add(pAssembly["version"].ToString());
-                        item.SubItems.Add(pAssembly["publickeytoken"].ToString());
-
-                        item.Tag = pAssembly.Id;
-
-                        listView_Assemblies.Items.Add(item);
-                    }
-                }
-            });
-        }
-
         private void tsbExportToDisk_Click(object sender, EventArgs e)
         {
             if (listView_Assemblies.CheckedItems.Count == 0)
             {
-                MessageBox.Show(this, "Please select at least one assembly in the list!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show(this, @"Please select at least one assembly in the list!", @"Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
             var fbDialog = new FolderBrowserDialog
             {
-                Description = "Select a location to recover the assemblies",
+                Description = @"Select a location to recover the assemblies",
                 ShowNewFolderButton = true
             };
 
@@ -153,13 +98,12 @@ namespace MsCrmTools.AssemblyRecoveryTool
                 {
                     foreach (ListViewItem item in listView_Assemblies.CheckedItems)
                     {
-                        string filename = string.Format("{0}{1}.dll",
-                            (fbDialog.SelectedPath.EndsWith("\\")
-                            ? fbDialog.SelectedPath
-                            : fbDialog.SelectedPath + "\\"),
-                        item.Text);
+                        var assembly = (Entity)item.Tag;
 
-                        byte[] buffer = Manager.RetrieveAssembly((Guid)item.Tag);
+                        var filename = $"{item.Text}_{assembly.GetAttributeValue<string>("version")}.dll";
+                        filename = Path.Combine(fbDialog.SelectedPath, filename);
+
+                        byte[] buffer = manager.RetrieveAssembly(assembly.Id);
 
                         using (var writer = new BinaryWriter(File.Open(filename, FileMode.Create)))
                         {
@@ -167,7 +111,7 @@ namespace MsCrmTools.AssemblyRecoveryTool
                         }
                     }
 
-                    MessageBox.Show(this, "Assemblies recovered!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show(this, @"Assemblies recovered!", @"Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
@@ -176,5 +120,40 @@ namespace MsCrmTools.AssemblyRecoveryTool
         {
             ExecuteMethod(RetrieveAssemblies);
         }
+
+        #endregion Events
+
+        #region Methods
+
+        public void RetrieveAssemblies()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Loading assemblies...",
+                Work = (bw, e) =>
+                {
+                    manager = new AssemblyManager(Service);
+                    e.Result = manager.RetrieveAssemblies();
+                },
+                PostWorkCallBack = e =>
+                {
+                    listView_Assemblies.Items.Clear();
+
+                    var list = (List<Entity>)e.Result;
+
+                    foreach (Entity pAssembly in list)
+                    {
+                        var item = new ListViewItem(pAssembly.GetAttributeValue<string>("name"));
+                        item.SubItems.Add(pAssembly.GetAttributeValue<string>("version"));
+                        item.SubItems.Add(pAssembly.GetAttributeValue<string>("publickeytoken"));
+                        item.Tag = pAssembly;
+
+                        listView_Assemblies.Items.Add(item);
+                    }
+                }
+            });
+        }
+
+        #endregion Methods
     }
 }
